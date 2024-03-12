eb5cbe6aa11d2424e1b6f884cbfec150a813624a
wb89rook <58337713+wb89rook@users.noreply.github.com>
2024-03-12 16:07:43 +0300

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,13 @@
+import requests
+
+weather_api_key = "e9d7239a61fade4678c26fe1059913d3"
+
+city = "Taganrog"
+
+url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={weather_api_key}'
+
+res = requests.get(url)
+data = res.json()
+
+for forecast in data['list']:
+    print(f"Время: {forecast['dt_txt']}, Температура воздуха: {round(forecast['main']['temp'] - 273, 1)} °C, Скорость ветра: {round(forecast['wind']['speed'], 2)} м/с, Давление: {round(forecast['main']['pressure'] * 0.75)} мм рт. ст., Влажность: {forecast['main']['humidity']} %, Погода: {forecast['weather'][0]['description']}\n\n")
