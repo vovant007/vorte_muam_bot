aba4ec696f5eeabfa95975f0c60ac401a027c5fe
aestlate <107574745+aestlate@users.noreply.github.com>
2024-04-14 21:49:33 +0300

    Add files via upload
    Подфайл функции вывода фото с адресами бота

---
+++ location.py
@@ -0,0 +1,40 @@
+n=('ТРЦ Мармелад', #0
+   'Приморский парк', #1
+   'Сквер Петра I', #2
+   'Каменная лестница', #3
+   'Пушкинская набережная', #4
+   'Домик Чехова', #5
+   'Парк Культуры и отдыха имени Горького', #6
+   'Музей Лавка Чеховых', #7
+   'Литературный Музей А.П. Чехова', #8
+   'Таганрогский военно-исторический музей', #9
+   'Свято-Никольский храм', #10
+   'Таганрогский краеведческий музей') #11
+urlloc=('https://www.malls.ru/upload/resize_cache/iblock/6ef/632_700_19c6be600154fb3b2d6e1ab89c139c97e/img_9499.jpg', #0
+        'https://avatars.mds.yandex.net/get-altay/10700016/2a0000018afc20db84d0d11c507ded49e73b/L_height', #1
+        'https://avatars.mds.yandex.net/get-altay/9742646/2a00000188b6524301777691144d90ecc277/L_height', #2
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/04/55/41/cc/caption.jpg?w=400&h=-1&s=1', #3
+        'https://visitdon.ru/upload/resize_cache/iblock/618/1218_445_1/utregw3mvlxejv5bswfq1o6mt97frx7a.jpg', #4
+        'https://www.culture.ru/_next/image?url=https%3A%2F%2Fcdn.culture.ru%2Fimages%2F15b2f8b2-d1a9-526d-a74e-85c23c3a3626%2Fw_884%2Ch_442%2Cc_fill%2Cg_center%2F7f1d5b34e86705d7d292d9000f32645d-jpg&w=1920&q=75', #5
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/3a/76/5d/caption.jpg?w=1200&h=-1&s=1', #6
+        'https://img.tourister.ru/files/3/2/5/0/0/1/3/7/original.jpg', #7
+        'https://www.culture.ru/_next/image?url=https%3A%2F%2Fcdn.culture.ru%2Fimages%2Fdb501dbc-3f04-555f-ad19-3a4c14b19458%2Fw_884%2Ch_442%2Cc_fill%2Cg_center%2F906fd1753f17f5bf1fcf483a09c368b5-jpeg&w=1920&q=75', #8
+        'https://avatars.mds.yandex.net/get-altay/9717139/2a00000188f44620f50be0e3b301191a3883/L_height', #9
+        'https://photos.wikimapia.org/p/00/00/15/72/94_big.jpg', #10
+        'https://rossaprimavera.ru/static/files/6b0426799d99.jpg') #11
+map=('https://yandex.ru/maps/-/CDRl6D34', #0
+     'https://yandex.ru/maps/-/CDRl6PNq', #1
+     'https://yandex.ru/maps/-/CDRl6P6j', #2
+     'https://yandex.ru/maps/-/CDRl6PjL', #3
+     'https://yandex.ru/maps/-/CDRl6P7y', #4
+     'https://yandex.ru/maps/-/CDVEmNJ1', #5
+     'https://yandex.ru/maps/-/CDVEmR2x', #6
+     'https://yandex.ru/maps/-/CDVEmK2f', #7
+     'https://yandex.ru/maps/-/CDVEmOlM', #8
+     'https://yandex.ru/maps/-/CDVEmSpq', #9
+     'https://yandex.ru/maps/-/CDVEmWlQ', #10
+     'https://yandex.ru/maps/-/CDVEm00c') #11
+
+
+
+
